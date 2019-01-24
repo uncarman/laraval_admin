ef842cc872f1ce4984c1ff1d8b56f543f3967a25
wang <sam.wan@sunallies.com>
2019-01-24 19:43:38 +0800

    优化登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,6 +10,7 @@
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
         <!-- Navbar Right Menu -->
+        @if(!empty(request()->user()))
         <ul class="nav nav-pills pull-left">
             <li class="btn-group active">
                 <a href="/monitor/summary" class=""><span class="glyphicon glyphicon-dashboard"></span> 监测分析</a>
@@ -79,19 +80,32 @@
                 </ul>
             </li>
         </ul>
+        @endif
 
+        @if(!empty(request()->user()))
         <ul class="nav navbar-nav pull-right">
             <li class="dropdown btn-group user user-menu">
                 <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="/admin-lte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> 旺财
+                    <img src="/admin-lte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> {{ request()->user()->name }}
                     <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a href="/monitor/ammeter">个人设置</a></li>
-                    <li><a href="/monitor/watermeter">退出登录</a></li>
+                    <li><a href="/profile">个人设置</a></li>
+                    <li><a onclick="event.preventDefault(); document.getElementById('logout-form').submit();">退出登录</a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                          style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 </ul>
             </li>
         </ul>
+        @else
+        <ul class="nav navbar-nav pull-right">
+            <li class="dropdown btn-group user user-menu">
+                <a href="login">用户登录</a>
+            </li>
+        </ul>
+        @endif
 
     </nav>
 
